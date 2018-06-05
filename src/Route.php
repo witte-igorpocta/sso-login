0da849f3ce2671b72c99dd7cb537515c9c8e17b9
Igor Pocta <igor.pocta@witte-automotive.cz>
2018-06-05 23:36:00 +0200

    Uprava routy

--- src/Route.php
+++ src/Route.php
@@ -7,7 +7,7 @@ class Route
     public static function create()
     {
         $routeList = new \Nette\Application\Routers\RouteList('WitteLogin');
-        $routeList[] = new \Nette\Application\Routers\Route('sso/<action>[/<id>]', 'SingleSignOn:default');
+        $routeList[] = new \Nette\Application\Routers\Route('ssologin/<action>[/<id>]', 'SingleSignOn:default');
         return $routeList;
     }
 }
